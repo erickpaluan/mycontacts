eaae8ac1f8a167e100fa5a27a732bae358df0ebd
Erick Paluan <epaluan@outlook.com>
2024-02-26 21:15:47 -0300

    middlewares

--- src/routes.js
+++ src/routes.js
@@ -4,7 +4,14 @@ const ContactController = require('./app/Controllers/ContactController');
 
 const router = Router();
 
-router.get('/contacts', ContactController.index);
+router.get(
+  '/contacts',
+  (request, response, next) => {
+    request.appID = 'MeuAppID';
+    next();
+  },
+  ContactController.index,
+);
 router.get('/contacts/:id', ContactController.show);
 router.delete('/contacts/:id', ContactController.delete);
 
